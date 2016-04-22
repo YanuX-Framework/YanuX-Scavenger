a46e36e88624c218d89b888ab9c86e5547724b9d
petersaints <petersaints@gmail.com>
2016-04-22 12:17:22 +0100

    - Added a few more things to the "pt.unl.fct.di.novalincs.yanux.scavenger.common.cell" package     * Actually use and finish the implementation of this package

--- common/src/main/java/pt/unl/fct/di/novalincs/yanux/scavenger/common/cell/CellInfoCollector.java
+++ common/src/main/java/pt/unl/fct/di/novalincs/yanux/scavenger/common/cell/CellInfoCollector.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.telephony.CellInfo;
 import android.telephony.CellLocation;
+import android.telephony.NeighboringCellInfo;
 import android.telephony.TelephonyManager;
 
 import java.util.List;
 
 import pt.unl.fct.di.novalincs.yanux.scavenger.common.permissions.PermissionManager;
 
+//TODO: Actually use and finish implementation
 public class CellInfoCollector {
     private final Context context;
     private final TelephonyManager telephonyManager;
@@ -27,7 +29,44 @@ public class CellInfoCollector {
         return telephonyManager.getAllCellInfo();
     }
 
+    public List<NeighboringCellInfo> getNeighboringCellInfo() {
+        return telephonyManager.getNeighboringCellInfo();
+    }
+
     public CellLocation getCellLocation() {
         return telephonyManager.getCellLocation();
     }
+
+    public int getNetworkType() {
+        return telephonyManager.getNetworkType();
+    }
+
+    public String getNetworkOperatorName() {
+        return telephonyManager.getNetworkOperatorName();
+    }
+
+    public String getNetworkOperator() {
+        return telephonyManager.getNetworkOperator();
+    }
+
+    public boolean isNetworkRoaming() {
+        return telephonyManager.isNetworkRoaming();
+    }
+
+    public int getSimState() {
+        return telephonyManager.getSimState();
+    }
+
+    public String getSimOperator() {
+        return telephonyManager.getSimOperator();
+    }
+
+    public String getSimOperatorName() {
+        return telephonyManager.getSimOperatorName();
+    }
+
+    public String getSimCountryIso() {
+        return telephonyManager.getSimCountryIso();
+    }
+
 }
---
+++ common/src/main/java/pt/unl/fct/di/novalincs/yanux/scavenger/common/cell/NetworkInfo.java
@@ -0,0 +1,4 @@
+package pt.unl.fct.di.novalincs.yanux.scavenger.common.cell;
+
+public class NetworkInfo {
+}
